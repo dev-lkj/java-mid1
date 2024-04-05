a95d8fe89e003739949a370810f737bd4f7ab7fa
dev-lkj <2_kj@daum.net>
2024-04-05 21:55:30 +0900

    String practice add

---
+++ src/lang/string/equals/StringEqualsMain1.java
@@ -0,0 +1,14 @@
+package lang.string.equals;
+
+public class StringEqualsMain1 {
+    public static void main(String[] args) {
+        String str1 = new String("hello"); //x001
+        String str2 = new String("hello"); //x002
+        System.out.println("new String() == 비교: " + (str1 == str2));
+        System.out.println("new String() equals 비교: " + (str1.equals(str2)));
+        String str3 = "hello"; //x003
+        String str4 = "hello"; //x004? 문자열풀에서 찾아서 주소값 가져옴
+        System.out.println("리터럴 == 비교: " + (str3 == str4));
+        System.out.println("리터럴 equals 비교: " + (str3.equals(str4)));
+    }
+}
